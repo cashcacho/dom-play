12bd2e093ced48afc614939653b7dd1689331f3e
Justine Cacho <justinecacho@gmail.com>
2023-07-06 23:49:35 -0700

    update character lines

--- js/dom-play.js
+++ js/dom-play.js
@@ -15,3 +15,39 @@
 */
 
 //document.querySelector(".hamlet").style.backgroundColor = "yellow";
+
+function highlight (el){
+
+    if(el.style.backgroundColor == 'white' ){//orange
+        el.style.backgroundColor='orange';
+    }else{//white
+        el.style.backgroundColor='white';
+    }
+
+}
+
+function myAlert(){
+    alert("I'm clicked!")
+}
+
+function highlightActor(actor){
+    //alert ("Hi, I'm " + actor);
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor
+            mySpan.style.backgroundColor='orange';
+        }else{//other actor
+            mySpan.style.backgroundColor='white';
+        }
+    }
+}
+
+const spans = document.querySelectorAll("#play span");
+console.log(spans);
+
+for(const mySpan of spans){
+       // mySpan.addEventListener("click",myAlert);
+        mySpan.addEventListener("click",function(ev){
+            highlightActor(mySpan.dataset.actor);
+        });
+        //alert(mySpan.dataset.actor);
+    }
